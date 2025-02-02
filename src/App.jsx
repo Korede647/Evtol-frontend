bdae6b38f7d387d49c84e7008441fa9165f05df9
Korede Anunusor <koredeanunusor@gmai.com>
2025-02-02 21:12:40 +0100

    manage evtol

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import SideBar from './components/sidebar/SideBar'
+import MngEvtol from './pages/manageEvtol/MngEvtol'
 
 function App() {
   return (
     <>
     <Router>
       <Routes>
-         <Route path= "/" element = {<SideBar/>} />
+        <Route path='/' element = {<SideBar/>}/>
+         <Route path= "/manageEvtol" element = {<MngEvtol/>} />
       </Routes>
     </Router>
 
---
+++ src/pages/manageEvtol/MngEvtol.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./mngEvtol.css"
+import SideBar from '../../components/sidebar/SideBar'
+
+const MngEvtol = () => {
+  return (
+    <div className='mngMain'>
+      <SideBar/>
+      <div className="mng">
+         <div className="text">
+            <h3>All Evtol Devices and their features</h3>
+         </div>
+      <table className='tab'>
+       <tr className='thead'>
+        <th>Serial No</th>
+        <th>Model</th>
+        <th>Weight Limit</th>
+        <th>Battery Capacity</th>
+        <th>Status</th>
+       </tr>
+
+        <tr className='evtol'>
+            <td>ENENNFJJKKSK</td>
+            <td>CRUISERWEIGHT</td>
+            <td>400.0</td>
+            <td>90%</td>
+            <td> DELIVERING</td>
+            </tr>
+      </table>
+
+      </div>
+    </div>
+  )
+}
+
+export default MngEvtol
